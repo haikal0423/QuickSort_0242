9de554e140aa184f3977bcba507820411f57b700
haikal0423 <mhaikal23azka@gmail.com>
2024-03-28 14:56:10 +0700

    Menambahkan step 11

--- ASDQuickSort/ASDQuickSort.cpp
+++ ASDQuickSort/ASDQuickSort.cpp
@@ -66,6 +66,16 @@ void q_short(int low, int high)
         }
         cmp_count++;
         if (i < j) //step 9
+        {
+            //swap the element at index i whit the element at index j 
+            swap(i, j);
+        }
+    }
+    if (low < j) { //step 11
+        //swap the pivot element with the element at index j 
+        swap(low, j);
+    }
+
 
 
 
